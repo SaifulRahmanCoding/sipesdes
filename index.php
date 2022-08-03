314e8b9655f3b3935084e39be1914105079d35ae
Saiful Rahman <sr.perante@gmail.com>
2022-08-03 11:46:57 +0700

    update

--- index.php
+++ index.php
@@ -4,31 +4,35 @@ require_once('cek-login.php');
 
 //cek apakah sudah login
 //jika belum login, akan di lempar ke form login 
-if ($sessionStatus==false) { header("Location: login.php"); }
+if ($sessionStatus == false) {
+	header("Location: login.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Dashboard</title>
-	<?php 
+	<?php
 	require('config/script.php');
-	require('config/style.php'); 
+	require('config/style.php');
 	?>
 </head>
+
 <body>
 	<!-- bagian sidebar -->
 	<?php require('komponen/sidebar.php'); ?>
 
 	<!-- header-->
-	<?php require('komponen/header-konten.php');?>
+	<?php require('komponen/header-konten.php'); ?>
 
 	<div id="bungkus" class="d-flex">
 
 		<!-- bagian konten -->
 		<div id="konten">
-			
+
 			<!-- isi konten -->
 			<div class="isi-konten m-3">
 				<h4 class="fw-bolder text-center mt-4 mb-4">Sistem Informasi Pelayanan Surat Desa Bantal</h4>
@@ -43,26 +47,26 @@ if ($sessionStatus==false) { header("Location: login.php"); }
 						$no_surat = mysqli_fetch_assoc($result);
 						if (is_null($no_surat)) {
 							$no_s = 0;
-						}else{
+						} else {
 							$no_s = intval($no_surat['no_surat']);
 						}
 						?>
-						<p class="m-0 fw-bolder fs-5"><?php echo $no_s?></p>
+						<p class="m-0 fw-bolder fs-5"><?php echo $no_s ?></p>
 						<p class="m-0">Surat Dibuat</p>
 					</div>
-					
+
 					<!-- batas -->
 					<div class="box-dashboard col-2 bg-white p-3 my-3 mx-4 shadow-sm text-center">
 						<?php
 						$query = "SELECT * FROM tb_penduduk";
 						$result = mysqli_query($db, $query);
-						
+
 						$jml_penduduk = mysqli_num_rows($result);
 						?>
-						<p class="m-0 fw-bolder fs-5"><?php echo $jml_penduduk;?></p>
+						<p class="m-0 fw-bolder fs-5"><?php echo $jml_penduduk; ?></p>
 						<p class="m-0">Penduduk</p>
 					</div>
-					
+
 					<!-- batas -->
 					<div class="box-dashboard col-2 bg-white p-3 my-3 mx-4 shadow-sm text-center">
 						<?php
@@ -71,10 +75,10 @@ if ($sessionStatus==false) { header("Location: login.php"); }
 
 						$jml_L = mysqli_num_rows($result);
 						?>
-						<p class="m-0 fw-bolder fs-5"><?php echo $jml_L;?></p>
+						<p class="m-0 fw-bolder fs-5"><?php echo $jml_L; ?></p>
 						<p class="m-0">Laki - Laki</p>
 					</div>
-					
+
 					<!-- batas -->
 					<div class="box-dashboard col-2 bg-white p-3 my-3 mx-4 shadow-sm text-center">
 						<?php
@@ -83,10 +87,10 @@ if ($sessionStatus==false) { header("Location: login.php"); }
 
 						$jml_P = mysqli_num_rows($result);
 						?>
-						<p class="m-0 fw-bolder fs-5"><?php echo $jml_P;?></p>
+						<p class="m-0 fw-bolder fs-5"><?php echo $jml_P; ?></p>
 						<p class="m-0">Perempuan</p>
 					</div>
-					
+
 					<!-- batas -->
 					<div class="box-dashboard col-2 bg-white p-3 my-3 mx-4 shadow-sm text-center">
 						<?php
@@ -96,7 +100,7 @@ if ($sessionStatus==false) { header("Location: login.php"); }
 
 						$jml_kk = $kk['jml_kk'];
 						?>
-						<p class="m-0 fw-bolder fs-5"><?php echo $jml_kk;?></p>
+						<p class="m-0 fw-bolder fs-5"><?php echo $jml_kk; ?></p>
 						<p class="m-0">Kepala Keluarga</p>
 					</div>
 
@@ -112,4 +116,5 @@ if ($sessionStatus==false) { header("Location: login.php"); }
 		<!-- end konten -->
 	</div>
 </body>
+
 </html>
